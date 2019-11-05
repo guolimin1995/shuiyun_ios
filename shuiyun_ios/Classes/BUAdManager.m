23dfb6a6978e0e47d3ca9dcf746ff21dd376a4f3
6cit-cheman <cheman@6cit.com>
2019-11-05 17:00:52 +0800

    fix

--- shuiyun_ios/Classes/BUAdManager.m
+++ shuiyun_ios/Classes/BUAdManager.m
@@ -38,7 +38,7 @@ NSString *videoKey = @"";
 
 
 // 显示激励视频
--(void)showRewardVideoAd: success:(void (^)(NSString *))success failure:(void (^)(NSString *))failure{
+-(void)showRewardVideoAd:(void (^)(NSString *))success failure:(void (^)(NSString *))failure{
     if (!self.rewardVideoAd) {
         self.rewardVideoAd = [BUAdRewardedVideo new];
         //[[self getRootViewController].view addSubview:self.rewardVideoAd.view];
@@ -70,44 +70,44 @@ NSString *videoKey = @"";
 }
 
 
-// //加载广告
-// -(void)loadNativeAd:(NSString *)type success:(void (^)(NSString *))success failure:(void (^)(NSString *))failure{
-//     //    NSLog(@"------ 加载广告 %@",type);
-//     NSArray *aName = [NSArray arrayWithObjects:@"bannerAd",@"videoAd",nil];
-//     int index = (int)[aName indexOfObject:type];
-//     switch (index) {
-//         case 0:         //banner广告
-//         {
-//             if (!self.bannerAd) {
-//                 self.bannerAd = [BUAdBanner new];
-//                 [[self getRootViewController].view addSubview:self.bannerAd.view];
-//                 //                [[self getRootViewController].navigationController pushViewController:self.bannerAd.view animated:YES];
-//             }
-//             [self.bannerAd loadBannerAd];       //加载banner广告
-//             break;
-//         }
-//         case 1:         //激励视频
-//         {
-//             if (!self.rewardVideoAd) {
-//                 self.rewardVideoAd = [BUAdRewardedVideo new];
-//                 //[[self getRootViewController].view addSubview:self.rewardVideoAd.view];
-//                 [[self getRootViewController].navigationController pushViewController:self.rewardVideoAd animated:YES];
-//             }
+//加载广告
+-(void)loadNativeAd:(NSString *)type success:(void (^)(NSString *))success failure:(void (^)(NSString *))failure{
+    //    NSLog(@"------ 加载广告 %@",type);
+    NSArray *aName = [NSArray arrayWithObjects:@"bannerAd",@"videoAd",nil];
+    int index = (int)[aName indexOfObject:type];
+    switch (index) {
+        case 0:         //banner广告
+        {
+            if (!self.bannerAd) {
+                self.bannerAd = [BUAdBanner new];
+                [[self getRootViewController].view addSubview:self.bannerAd.view];
+                //                [[self getRootViewController].navigationController pushViewController:self.bannerAd.view animated:YES];
+            }
+            [self.bannerAd loadBannerAd];       //加载banner广告
+            break;
+        }
+        case 1:         //激励视频
+        {
+            if (!self.rewardVideoAd) {
+                self.rewardVideoAd = [BUAdRewardedVideo new];
+                //[[self getRootViewController].view addSubview:self.rewardVideoAd.view];
+                [[self getRootViewController].navigationController pushViewController:self.rewardVideoAd animated:YES];
+            }
             
-//             //加载激励视频广告
-//             [self.rewardVideoAd loadRewardedVideoAd:@"" :^(NSString *data) {
-//                 if ([data isEqualToString:@"success"]) {
-//                     success(nil);
-//                 }else{
-//                     failure(nil);
-//                 }
-//             }];
-//             break;
-//         }
-//         default:
-//         break;
-//     }
-// }
+            //加载激励视频广告
+            [self.rewardVideoAd loadRewardedVideoAd:@"" :^(NSString *data) {
+                if ([data isEqualToString:@"success"]) {
+                    success(nil);
+                }else{
+                    failure(nil);
+                }
+            }];
+            break;
+        }
+        default:
+        break;
+    }
+}
 
 // //
 // -(void)removeNativeAd:(NSString *)type success:(void (^)(NSString *))success failure:(void (^)(NSString *))failure{
